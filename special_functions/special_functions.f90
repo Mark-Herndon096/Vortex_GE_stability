1f188741319164c7dc960d2333f86008368018dc
markherndon <mah218@lehigh.edu>
2021-02-10 17:51:21 -0500

    New con

--- special_functions/special_functions.f90
+++ special_functions/special_functions.f90
@@ -1,6 +1,23 @@
 !=============================================================================
-! Written by Mark A. Herndon
-! Lehigh University, Department of Mechanical Engineering and Mechanics
+!   Author: Mark A. Herndon
+!   Lehigh University, Department of Mechanical Engineering and Mechanics
+!   
+!   Ideas, methods, and numerical constants based from the following
+!   References:
+!
+!   John F. Hart
+!   Computer Approximations
+!   year: 1978
+!   
+!   William H. Press, Saul A. Teuukolsky, William T. Vetterling, 
+!   Brian P. Flannery
+!   NUMERICAL RECIPES: The Art of Scientific Computing
+!   ISBN: 978-0-521-88068-8
+!
+!   Milton Abramowitz, Irene Stegun,
+!   Handbook of Mathematical Functions,
+!   National Bureau of Standards, 1964,
+!   ISBN: 0-486-61272-4,
 !=============================================================================
 FUNCTION BESSELJ0(x)
     USE mod_function_interface, ONLY : j01, j02, j03, j04, j05,  &
@@ -25,8 +42,31 @@ FUNCTION BESSELJ0(x)
         BESSELJ0 = SQRT(twoopi/ax)*(COS(xx)*np/dp - (8.d0/ax)*SIN(xx)*nq/dq)
     END IF
 END FUNCTION BESSELJ0
+!=============================================================================
+!=============================================================================
+FUNCTION BESSELJ1(x)
+    USE mod_function_interface, ONLY : j11, j12, j13, j14, j15,  &
+                                       j16,                      &
+                                       xj00, xj10, pio4, twoopi, &
+                                       RATIONAL_FIT,             &
+                                       ASYMPTOTIC_APPROX
+    IMPLICIT NONE
+    REAL(KIND=8), INTENT(INOUT) :: x
+    REAL(KIND=8)                :: BESSELJ0
+    REAL(KIND=8)                :: ax, xx, np, dp, nq, dq, y, ff
+    INTEGER                     :: n = 7
+    INTEGER                     :: m = 5
 
- 
+    IF ( ABS(x) .LE. 8.0 ) THEN
+        CALL RATIONAL_FIT(x, j11, j12, n, np, dp, y)
+        BESSELJ0 = np*(y-xj00)*(y-xj10)/dp
+    END IF
+    IF ( ABS(x) .GT. 8.0 ) THEN
+        ax = ABS(x); xx = ax - pio4; ff = 1.d0;
+        CALL ASYMPTOTIC_APPROX(x, j13, j14, j15, j16, m, np, dp, nq, dq, ff)
+        BESSELJ1 = SQRT(twoopi/ax)*(COS(xx)*np/dp - (8.d0/ax)*SIN(xx)*nq/dq)
+    END IF
+END FUNCTION BESSELJ1
 !=============================================================================
 !=============================================================================
 SUBROUTINE RATIONAL_FIT(x, r, s, n, np, dp, y)
